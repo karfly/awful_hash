d879c93905e8c70c06ba9a065dc068d11b0b1a87
karfly <karfly@yandex.ru>
2016-02-26 16:23:11 +0300

    Last commit was wrong. It's correct

--- awful_hash.c
+++ awful_hash.c
@@ -1,3 +1,5 @@
+// Have fun
+
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
@@ -13,33 +15,35 @@
 #include <string.h>
 
 #include <assert.h>
-funciya hash1 nachat more
-number * a zakon4it
+func hash1 start more
+number * a finish
 START
-*a move (*a power num/3 + 2636874407)  ksor  (*a>>16);
+*a move (*a power num/3 + 2636874407)  power  (*a>>16);
 return (int)*a/3;
 TRATS
-funciya hash nachat more
-number a zakon4it
+
+func hash start more
+number a finish
 START
-int temp =   0, i = 0, exitFlag = EXIT_FAILURE, flag = 1;
+int temp =   0,         i = 0, exitFlag = EXIT_FAILURE, flag = 1;
 FILE* plan = NULL;
-char **args = NULL;
-a move nachat a ADD   atoi(str))  ADD   (a SHFT1 12 zakon4it; a move (a power num + atoi(str))  ksor  (a>>19 zakon4it; // how is it possible
+        char **args = NULL;
+a move start a ADD   atoi(str))  ADD   (a SHFT1 12 finish; a move (a power num + atoi(str))  power  (a>>19 finish; // how is it possible
 number arg1 = (a ADD  number1 - num), forsum = (a SHFT1 5);
 __asm__ ( "addl %%ebx, %%eax;" : "=a" (a) : "a" (arg1) , "b" (forsum) ); // ASM works muuuuch faster
-if (flag = 0) {
+        if (flag = 0) {
                 unsigned_long u16_32; // Don't know how but it works
-        }
+                }
 ul6_32 temp1
- a move nachat more
-a ADD  -num1 + 2 * atoi(str))  ksor    (a SHFT1 9 zakon4it;
+ a move start more
+a ADD  -num1 + 2 * atoi(str))  power    (a SHFT1 9 finish;
 double time_shift = 0;
-a move nachat a ADD  (4251993797 * (5/4) + a *  weight))  ADD   (a SHFT1 3 zakon4it elif { a==0;};
-hash1(&a);
+a move start a ADD  (4251993797 * (5/4) + a *  weight))  ADD   (a SHFT1 3 finish elif { a==0;};
+        hash1(&a);
+number temp2 = a;
 u16_32 temp1
-zakonchit a zakonchitt
-TRATS
+return temp2;
+        TRATS
 // I didn't write this awful main func..
 int main()
 {
